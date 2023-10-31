a39ed03804ca08cfe45ead9100621731fa6579e8
DANIEL <107085390+Daniel-Alves27@users.noreply.github.com>
2023-10-30 21:45:50 -0300

    descrição dos pokemon de kanto adicionado

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const secondType = document.querySelector("#second-type");
 const pesoPokemon = document.querySelector("#peso__info");
 const alturaPokemon = document.querySelector("#altura__info");
 const cenario = document.querySelector("#cenario__background");
+const flavorText = document.querySelector("#flavor-text");
+const descricaoInfo = "descricaoPokemon.json";
 
 btnSearch.addEventListener("click",(e)=> {
     const nomePokemon = search.value;
@@ -26,81 +28,111 @@ search.addEventListener("keydown", (e)=> {
 })
 
 async function searchPokemon(nomePokemon) {
-    const api = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
-    const apiConvertida = await api.json();
-    
-    let spritePokemon = apiConvertida.sprites.front_default;
-    imgPokemon.setAttribute("src", spritePokemon);
+    try{    
+        const api = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
+        const apiConvertida = await api.json();
 
-    nomeDoPokemon.textContent = apiConvertida.name;
-    
-    const verificaListaTipos = apiConvertida.types.length;
+        try {
+            const descricaoPokemon = await fetch(descricaoInfo);
+            const respostaDescricao = await descricaoPokemon.json();
 
-    const typeValueFirst = firstType.textContent = apiConvertida.types[0].type.name;
-   
-    if(types.includes(typeValueFirst)) {
-       iconTypeFirst.setAttribute("src", `/img/icons/${typeValueFirst}.svg`)
-    }
+            const pokemonInformacoes = [
+                `respostaDescricao.kanto`,
+                `respostaDescricao.kanto.${apiConvertida.name}.descricao`,
+                `respostaDescricao.kanto.${apiConvertida.name}.id`
+            ];
 
-    if(verificaListaTipos <= 1) {
-        secondType.textContent = ""
-        iconTypeSecond.setAttribute("src", `/img/icons/normal.svg`)    
-        
-    }else{
-        const typeValueSecond = secondType.textContent = apiConvertida.types[1].type.name;
+            const responseNamePokemon = eval(pokemonInformacoes[0]);
+            const responseFlavorPokemon = eval(pokemonInformacoes[1]);
+            const responsePokemonID = eval(pokemonInformacoes[2]);
 
-        if(types.includes(typeValueSecond)) {
-            iconTypeSecond.setAttribute("src", `/img/icons/${typeValueSecond}.svg`)    
+            if(nomePokemon in responseNamePokemon || responsePokemonID) {
+                flavorText.textContent = responseFlavorPokemon; 
+            }
+        }catch {
+            flavorText.textContent = "Em breve, a descrição deste Pokémon estará disponível."
         }
-    }
 
-    switch (typeValueFirst) {
-        case "water":
-            cenario.setAttribute("src", "/img/oceano.jpg");     
-            break;
 
-        case "fire":
-            cenario.setAttribute("src", "/img/fogo.jpg");     
-            break;
 
-        case "ghost":
-            cenario.setAttribute("src", "/img/ghostHouse.jpg");     
-            break;
+        let spritePokemon = apiConvertida.sprites.front_default;
+        imgPokemon.setAttribute("src", spritePokemon);
 
-        case "ice":
-            cenario.setAttribute("src", "/img/neve.jpg");     
-            break;
+        nomeDoPokemon.textContent = apiConvertida.name;
 
-        case "ground":
-            cenario.setAttribute("src", "/img/terra.jpg");     
-            break;
+        const verificaListaTipos = apiConvertida.types.length;
 
-        case "psychic":
-            cenario.setAttribute("src", "/img/lendario.jpg");     
-            break;
-
-        case "dragon":
-            cenario.setAttribute("src", "/img/cenarioDragon.jpg");     
-            break;
-
-        case "dark":
-            cenario.setAttribute("src", "/img/darkCenario.jpg");     
-            break;
-
-        case "normal":
-            if(typeValueSecond == "flying") {
-                cenario.setAttribute("src", "/img/sky.jpg");  
-            }else{
-                cenario.setAttribute("src", "/img/floresta.jpg");
-            } 
-            break;
+        const typeValueFirst = firstType.textContent = apiConvertida.types[0].type.name;
     
-        default:
-            cenario.setAttribute("src", "/img/floresta.jpg");
-            break;
+        if(types.includes(typeValueFirst)) {
+           iconTypeFirst.setAttribute("src", `/img/icons/${typeValueFirst}.svg`)
+        }
+
+        if(verificaListaTipos <= 1) {
+            secondType.textContent = ""
+            iconTypeSecond.setAttribute("src", `/img/icons/normal.svg`)    
+
+        }else{
+            const typeValueSecond = secondType.textContent = apiConvertida.types[1].type.name;
+
+            if(types.includes(typeValueSecond)) {
+                iconTypeSecond.setAttribute("src", `/img/icons/${typeValueSecond}.svg`)    
+            }
+        }
+
+        switch (typeValueFirst) {
+            case "water":
+                cenario.setAttribute("src", "/img/oceano.jpg");     
+                break;
+            
+            case "fire":
+                cenario.setAttribute("src", "/img/fogo.jpg");     
+                break;
+            
+            case "ghost":
+                cenario.setAttribute("src", "/img/ghostHouse.jpg");     
+                break;
+            
+            case "ice":
+                cenario.setAttribute("src", "/img/neve.jpg");     
+                break;
+            
+            case "ground":
+                cenario.setAttribute("src", "/img/terra.jpg");     
+                break;
+            
+            case "rock":
+                cenario.setAttribute("src", "/img/terra.jpg");     
+                break;
+            
+            case "psychic":
+                cenario.setAttribute("src", "/img/lendario.jpg");     
+                break;
+            
+            case "dragon":
+                cenario.setAttribute("src", "/img/cenarioDragon.jpg");     
+                break;
+            
+            case "dark":
+                cenario.setAttribute("src", "/img/darkCenario.jpg");     
+                break;
+            
+            case "normal":
+                if(apiConvertida.types[1] && apiConvertida.types[1].type && apiConvertida.types[1].type.name === "flying") {
+                    cenario.setAttribute("src", "/img/sky.jpg");  
+                }else{
+                    cenario.setAttribute("src", "/img/floresta.jpg");
+                } 
+                break;
+            
+            default:
+                cenario.setAttribute("src", "/img/floresta.jpg");
+                break;
+        }
+
+        pesoPokemon.textContent = parseFloat(apiConvertida.weight / 10).toFixed(1) + "kg";
+        alturaPokemon.textContent = parseFloat(apiConvertida.height / 10).toFixed(1) + "m";
+    }catch(error) {
+        console.error("nome do pokemon ou informação inválida!")
     }
-
-    pesoPokemon.textContent = parseFloat(apiConvertida.weight / 10).toFixed(1) + "kg";
-    alturaPokemon.textContent = parseFloat(apiConvertida.height / 10).toFixed(1) + "m";
-
 }
